7139f18e842fcef67340008f2cc1f014894e206c
Dmitry Kandratovich <boom@dell.localdomain>
2021-10-23 09:39:09 +0200

    edit class User

--- User.h
+++ User.h
@@ -9,12 +9,17 @@ private:
     std::string _name;
     bool _isAdmin;
 public:
-    //User() = default;
     User(const std::string& login, const std::string& password, const std::string& name, bool isAdmin = false) 
         : _login(login), _password(password), _name(name), _isAdmin(isAdmin) {}
+  
+    std::string getUserLogin() const { return _login; }
 
-    //User(User &other) : _login(other._login), _password(other._password), _name(other._name) {}
- 
-    const std::string& getUserLogin() const { return _login; }
-    const std::string& getUserName() const { return _name; }
+    std::string getUserPasword() const { return _password; }
+    void setUserPassword(const std::string& password) { _password = password; }
+
+    std::string getUserName() const { return _name; }
+    void setUserName(const std::string& name) { _name = name; }
+
+    bool getIsAdmin() const { return _isAdmin; }
+    void setIsAdmin(bool isAdmin) { _isAdmin = isAdmin; }
 };
